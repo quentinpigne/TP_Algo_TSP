a2069224483e9b14b583b24419cbacb21d986735
Quentin Pigné <quentin.pigne@ensimag.grenoble-inp.fr>
2014-10-21 10:40:27 +0200

    Adding start files

---
+++ src/TP_Algo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by pigneq on 21/10/14.
+ */
+
+public class TP_Algo {
+    public static main(String[] args) {
+
+    }
+}
---
+++ src/generator/GraphGenerator.java
@@ -0,0 +1,8 @@
+package generator;
+
+/**
+ * Created by pigneq on 21/10/14.
+ */
+
+public class GraphGenerator {
+}
