5bd4b6a0c56758f5c3e972017884215d714521e8
Quentin Pigné <quentin.pigne@ensimag.grenoble-inp.fr>
2014-11-12 13:33:33 +0100

    Ajout de quelques fonctions sur les arcs d'un graphe

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -7,25 +7,57 @@ public class Graph {
     private List<Vertex> V = new ArrayList<Vertex>();
     private List<Edge> E = new ArrayList<Edge>();
 
-    //Getters / Setters de sommets
-    public List<Vertex> getV() {
-        return V;
-    }
-
-    public Vertex getVertex(int i) {
-        return V.get(i);
-    }
-
+    /*
+        Fonctions sur les sommets
+     */
+    //Ajoute un sommet au graphe
     public void addVertex(Vertex vertex) {
         V.add(vertex);
     }
 
-    //Getters / Setters d'arc
+    //Permet de récupérer la liste des sommets
+    public List<Vertex> getV() {
+        return V;
+    }
+
+    //Permet de récupérer un sommet particulier
+    public Vertex getVertex(int i) {
+        return V.get(i);
+    }
+
+    /*
+        Fonctions sur les arcs
+     */
+    //Ajoute un arc au graphe
+    public void addEdge(Edge edge) {
+        E.add(edge);
+    }
+
+    //Permet de récupérer la liste des arcs
     public List<Edge> getE() {
         return E;
     }
 
-    public void addEdge(Edge edge) {
-        E.add(edge);
+    //Permet de récupérer l'arc reliant deux sommets
+    public Edge getEdge(Vertex v1, Vertex v2) {
+        for(Edge edge : E) {
+            if(edge.getStart() == v1 || edge.getStart() == v2) {
+                if(edge.getEnd() == v2 || edge.getEnd() == v1) {
+                    return edge;
+                }
+            }
+        }
+        return null;
+    }
+
+    //Permet de récupérer tous les arcs partants d'un sommet
+    public List<Edge> getVEdge(Vertex v) {
+        List<Edge> edgeList = new ArrayList<Edge>();
+        for(Edge edge : E) {
+            if(edge.getStart() == v) {
+                edgeList.add(edge);
+            }
+        }
+        return edgeList;
     }
 }
