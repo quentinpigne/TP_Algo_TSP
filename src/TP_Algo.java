6f2e4241402715d62e73562d9b449eb335cba97f
Quentin Pigné <quentin.pigne@gmail.com>
2014-11-13 00:02:58 +0100

    Continuation des algos Enumeration et Glouton

--- src/TP_Algo.java
+++ src/TP_Algo.java
@@ -1,3 +1,5 @@
+import algorithms.Enumeration;
+import algorithms.Glouton;
 import graph.BoundaryException;
 import graph.Graph;
 import graph.GraphGenerator;
@@ -9,10 +11,12 @@ public class TP_Algo {
     public static void main(String[] args) {
         Graph graphe = null;
         try {
-            graphe = GraphGenerator.generate(10, 0.1);
+            graphe = GraphGenerator.generate(4, 0.5);
         } catch (BoundaryException e) {
 
         }
+
+        System.out.println("TSP = " + Glouton.glouton(graphe).getLength());
 
         GraphGUI graphGUI = new GraphGUI(graphe.getV(), graphe.getE());
         graphGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/algorithms/Enumeration.java
+++ src/algorithms/Enumeration.java
@@ -1,5 +1,6 @@
 package algorithms;
 
+import graph.Edge;
 import graph.Graph;
 import graph.Path;
 import graph.Vertex;
@@ -9,6 +10,26 @@ import java.util.List;
 
 public class Enumeration {
     public static Path enumeration(Graph g) {
-        return null;
+        //Création de la liste des chemins
+        List<Path> listPath = new ArrayList<Path>();
+        List<Vertex> listVertex = new ArrayList<Vertex>();
+        findAllPaths(listPath, listVertex, g);
+
+        //Recherche du plus court chemin parmis tous ceux calculés
+        Path finalPath = listPath.get(0);
+        for(Path path : listPath) {
+            if(path.getLength() < finalPath.getLength()) finalPath = path;
+        }
+        return finalPath;
+    }
+
+    private static void findAllPaths(List<Path> listPath, List<Vertex> listVertex, Graph g) {
+        for(Vertex v : g.getV()) {
+            if(!listVertex.contains(v)) {
+                for(Edge edge : g.getVEdge(v)) {
+
+                }
+            }
+        }
     }
 }
