bbe5fdf415fd9ccc97cd3585dd95a708ecb1339b
Quentin Pigné <quentin.pigne@ensimag.grenoble-inp.fr>
2014-11-13 15:57:09 +0100

    Fin d'implementation de l'algo enumeration -> Fonctionnel

--- src/algorithms/Enumeration.java
+++ src/algorithms/Enumeration.java
@@ -7,6 +7,7 @@ import graph.Vertex;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /*
 Idée de l'algo :
@@ -17,37 +18,112 @@ Idée de l'algo :
  */
 
 public class Enumeration {
-    public static Path enumeration(Graph g) {
+    // pour stocker les sommets parcourus
+    private Stack<Integer> stack;
+    private int numberOfVertex;
+    private double[][] distanceMatrix;
+
+    public Stack<Integer> getStacks() {
+        return stack;
+    }
+
+    public int getNumberOfVertex() {
+        return numberOfVertex;
+    }
+
+    public double[][] getDistanceMatrix() {
+        return distanceMatrix;
+    }
+
+    public Enumeration(Graph graph){
+        stack = new Stack<Integer>();
+        numberOfVertex = graph.getV().size();
+        distanceMatrix = graph.getDistanceMatrix();
+    }
+
+    public Stack<Integer> solution() {
+        List<Stack<Integer>> stacks = new ArrayList<Stack<Integer>>();
+        for (int i = 0; i < factorielle(numberOfVertex); i++) {
+            stacks.add(new Stack<Integer>());
+        }
+
         //Création d'une liste de liste de sommets
-        List<List<Vertex>> vertexLists = new ArrayList<List<Vertex>>();
-        for (int i = 0; i < factorielle(g.getV().size()); i++) {
-            vertexLists.add(new ArrayList<Vertex>());
+        int vertexTab[] = new int[numberOfVertex];
+        for (int i = 0; i < vertexTab.length; i++) {
+            vertexTab[i] = i;
         }
 
         //Recherche des n combinaisons de n parmis n sommets
+        ArrayList<ArrayList<Integer>> combi = permute(vertexTab);
 
-
-        //Création de la liste de n! chemins
-        List<Path> listPath = new ArrayList<Path>();
-        for (int i = 0; i < factorielle(g.getV().size()); i++) {
-            listPath.add(new Path());
+        //Création de la liste des n! stacks
+        for (int i = 0; i < factorielle(numberOfVertex); i++) {
+            for (int j = 0; j < numberOfVertex; j++) {
+                stacks.get(i).push(combi.get(i).get(j));
+            }
         }
-
-        //Remplissage des chemins pour les n! listes de sommets
 
         //Recherche du plus court chemin parmis tous ceux calculés
-        Path finalPath = listPath.get(0);
-        for(Path path : listPath) {
-            if(path.getLength() < finalPath.getLength()) finalPath = path;
+        stack = stacks.get(0);
+        for(Stack<Integer> st : stacks) {
+            if(cost(st) < cost(stack)) stack = st;
         }
-        return finalPath;
+        return stack;
     }
 
-    private static int factorielle(int n)
+    public double cost(){
+        double d = 0.0;
+        for (int i = 0; i < stack.size()  ; i++) {
+            d += distanceMatrix[stack.elementAt(i % (numberOfVertex) )][stack.elementAt((i+1) % numberOfVertex )];
+        }
+        return d;
+    }
+
+    private double cost(Stack<Integer> s){
+        double d = 0.0;
+        for (int i = 0; i < stack.size()  ; i++) {
+            d += distanceMatrix[s.elementAt(i % (numberOfVertex) )][s.elementAt((i+1) % numberOfVertex )];
+        }
+        return d;
+    }
+
+    private int factorielle(int n)
     {
         if(n>1)
             return n*factorielle(n - 1);
         else
             return 1;
+    }
+
+    private ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+        //start from an empty list
+        result.add(new ArrayList<Integer>());
+
+        for (int i = 0; i < num.length; i++) {
+            //list of list in current iteration of the array num
+            ArrayList<ArrayList<Integer>> current = new ArrayList<ArrayList<Integer>>();
+
+            for (ArrayList<Integer> l : result) {
+                // # of locations to insert is largest index + 1
+                for (int j = 0; j < l.size()+1; j++) {
+                    // + add num[i] to different locations
+                    l.add(j, num[i]);
+
+                    ArrayList<Integer> temp = new ArrayList<Integer>(l);
+                    current.add(temp);
+
+                    //System.out.println(temp);
+
+                    // - remove num[i] add
+                    l.remove(j);
+                }
+            }
+
+            result = new ArrayList<ArrayList<Integer>>(current);
+        }
+
+        return result;
     }
 }
