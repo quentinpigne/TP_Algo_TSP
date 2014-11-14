0b43294804ddb6ab0b8bbe90690cdc13b530b1af
Quentin Pigné <quentin.pigne@ensimag.grenoble-inp.fr>
2014-11-14 13:21:00 +0100

    Modification de la classe principale -> mesure du temps d'execution en fonction de la taille

--- src/TP_Algo.java
+++ src/TP_Algo.java
@@ -1,5 +1,5 @@
+import algorithms.Enumeration;
 import graph.BoundaryException;
-import algorithms.Glouton;
 import graph.Graph;
 import graph.GraphGenerator;
 import gui.GraphGUI;
@@ -9,26 +9,28 @@ import java.util.Stack;
 
 public class TP_Algo {
     public static void main(String[] args) {
-        Graph graphe = null;
-        try {
-            graphe = GraphGenerator.generate(6, 0.1);
-        } catch (BoundaryException e) {
-
+        long times[] = new long[10];
+        for (int i = 1; i <= 10; i++) {
+            Graph graphe = null;
+            try {
+                graphe = GraphGenerator.generate(i, 0.2);
+            } catch (BoundaryException e) {}
+            long timeBefore = System.currentTimeMillis();
+            Enumeration enumeration = new Enumeration(graphe);
+            enumeration.solution();
+            long timeAfter = System.currentTimeMillis();
+            long timeExec = timeAfter - timeBefore;
+            times[i-1] = timeExec;
         }
 
-        GraphGUI graphGUI = new GraphGUI(graphe.getV(), graphe.getE());
-        graphGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        graphGUI.setSize(750, 750);
-        graphGUI.setVisible(true);
+        //Affichage des temps
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Graphe avec " + i + " sommet(s) -> " + times[i-1] + " ms.");
+        }
 
-        Glouton glouton = new Glouton(graphe);
-        Stack<Integer> stack = new Stack<Integer>();
-        stack = glouton.solution(4);
-//        for (int i = 0; i < stack.size(); i++) {
-//            System.out.println(stack.elementAt(i));
-//        }
-        double d = glouton.cost();
-        //System.out.println(d);
-
+//        GraphGUI graphGUI = new GraphGUI(graphe.getV(), graphe.getE());
+//        graphGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        graphGUI.setSize(750, 750);
+//        graphGUI.setVisible(true);
     }
 }
