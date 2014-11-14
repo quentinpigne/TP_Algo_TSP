94370b1d92fdca619958004cbe46608cd56191bc
Quentin Pigné <quentin.pigne@ensimag.grenoble-inp.fr>
2014-11-14 16:17:13 +0100

    Mise en page et commentaires du code de l'algo enumeration

--- src/algorithms/Enumeration.java
+++ src/algorithms/Enumeration.java
@@ -1,9 +1,6 @@
 package algorithms;
 
-import graph.Edge;
 import graph.Graph;
-import graph.Path;
-import graph.Vertex;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +10,11 @@ import java.util.Stack;
 Idée de l'algo :
 - Trouver les n combinaisons de n parmis n sommets
 - Créer une liste de n! chemins
-- Remplir les chemins avec les arcs -> calcul des longueurs
+- Récupérer les longueurs des n! chemins à l'aide de la matrice des distances
 - Trouver le chemin de longueur minimale
  */
 
 public class Enumeration {
-    // pour stocker les sommets parcourus
     private Stack<Integer> stack;
     private int numberOfVertex;
     private double[][] distanceMatrix;
@@ -42,21 +38,22 @@ public class Enumeration {
     }
 
     public Stack<Integer> solution() {
+        //On crée la liste qui servira à stocker toutes les combinaisons de chemins possibles
         List<Stack<Integer>> stacks = new ArrayList<Stack<Integer>>();
         for (int i = 0; i < factorielle(numberOfVertex); i++) {
             stacks.add(new Stack<Integer>());
         }
 
-        //Création d'une liste de liste de sommets
+        //Création du vecteur contenant tous les sommets du graphe
         int vertexTab[] = new int[numberOfVertex];
         for (int i = 0; i < vertexTab.length; i++) {
             vertexTab[i] = i;
         }
 
-        //Recherche des n combinaisons de n parmis n sommets
+        //Recherche des n combinaisons de n parmis n sommets à partir du vecteur des sommets du graphe
         ArrayList<ArrayList<Integer>> combi = permute(vertexTab);
 
-        //Création de la liste des n! stacks
+        //Remplissage de la liste des n! chemins possibles
         for (int i = 0; i < factorielle(numberOfVertex); i++) {
             for (int j = 0; j < numberOfVertex; j++) {
                 stacks.get(i).push(combi.get(i).get(j));
@@ -68,9 +65,13 @@ public class Enumeration {
         for(Stack<Integer> st : stacks) {
             if(cost(st) < cost(stack)) stack = st;
         }
+
         return stack;
     }
 
+    /*
+    Fonction renvoyant la longueur du plus petit chemin calculé par l'algorithme
+     */
     public double cost(){
         double d = 0.0;
         for (int i = 0; i < stack.size()  ; i++) {
@@ -79,6 +80,9 @@ public class Enumeration {
         return d;
     }
 
+    /*
+    Fonction calculant la longueur d'un chemin (un ensemble de sommets).
+     */
     private double cost(Stack<Integer> s){
         double d = 0.0;
         for (int i = 0; i < stack.size()  ; i++) {
@@ -87,6 +91,9 @@ public class Enumeration {
         return d;
     }
 
+    /*
+    Fonction renvoyant la factorielle d'un nombre
+     */
     private int factorielle(int n)
     {
         if(n>1)
@@ -95,35 +102,26 @@ public class Enumeration {
             return 1;
     }
 
+    /*
+    Fonction permettant de trouver toutes les permutations possibles de n nombres.
+     */
     private ArrayList<ArrayList<Integer>> permute(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-
-        //start from an empty list
         result.add(new ArrayList<Integer>());
 
         for (int i = 0; i < num.length; i++) {
-            //list of list in current iteration of the array num
             ArrayList<ArrayList<Integer>> current = new ArrayList<ArrayList<Integer>>();
 
             for (ArrayList<Integer> l : result) {
-                // # of locations to insert is largest index + 1
                 for (int j = 0; j < l.size()+1; j++) {
-                    // + add num[i] to different locations
                     l.add(j, num[i]);
-
                     ArrayList<Integer> temp = new ArrayList<Integer>(l);
                     current.add(temp);
-
-                    //System.out.println(temp);
-
-                    // - remove num[i] add
                     l.remove(j);
                 }
             }
-
             result = new ArrayList<ArrayList<Integer>>(current);
         }
-
         return result;
     }
 }
